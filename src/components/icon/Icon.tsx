5b6c7259a0c965cef977943b4b48f594136c031b
Aliaksei Holad <aleksei-golod@mail.ru>
2024-05-23 22:40:26 +0300

    Create header and add images

---
+++ src/components/icon/Icon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import iconsSprite from '../../assets/images/icons-sprite.svg'
+
+type IconPropsType = {
+    iconId: string
+    width?: string
+    height?: string
+    viewBox?: string
+}
+
+export const Icon = (props: IconPropsType) => {
+    return (
+
+        <svg width={props.width || "39"} height={props.height || "38"} viewBox={props.viewBox || "0 0 39 38"}
+             fill="none" xmlns="http://www.w3.org/2000/svg">
+            <use xlinkHref={`${iconsSprite}#${props.iconId}`}/>
+        </svg>
+
+    );
+};
